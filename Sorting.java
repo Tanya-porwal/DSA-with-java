ec6fddf7baecab1db38b59515645315ee42c5dea
Tanya <tanyaporwal9027l@gmail.com>
2026-04-02 22:01:31 +0530

    Learned new Sorting methods

--- Sorting.java
+++ Sorting.java
@@ -8,7 +8,15 @@ public class Sorting {
         System.out.println("array for  bubble sort:");
         printArray(arr);
 
-        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp ;
+                }
+            }
+        }
         System.out.println();
         System.out.println("Array after bubble sort: ");
         printArray(arr);
@@ -19,7 +27,19 @@ public class Sorting {
         System.out.println();
         System.out.println("array for  selection sort:");
         printArray(arr);
-
+        for(int i=0;i<arr.length;i++){
+            int smallest=i ;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[smallest]){
+                    smallest=j;
+                }
+            }
+            if(i!=smallest){
+                int temp=arr[i];
+                arr[i]=arr[smallest];
+                arr[smallest]=temp;
+            }
+        }
 
         System.out.println();
         System.out.println("Array after selection sort: ");
@@ -31,6 +51,15 @@ public class Sorting {
         System.out.println();
         System.out.println("array for insertion sort:");
         printArray(arr);
+        for(int i=1;i<arr.length;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        } 
 
 
         System.out.println();
